b6e85df6dd84ec45409f2aa7af4e6cf28f0f6f8f
Gavin Button <shop@dratson.co.uk>
2018-01-14 13:00:02 +0000

    updated combo box bound value

--- WpfLayoutTest/ModuleA/ViewModels/RibbonViewModel.cs
+++ WpfLayoutTest/ModuleA/ViewModels/RibbonViewModel.cs
@@ -7,9 +7,13 @@ namespace ModuleA.ViewModels
 {
     class RibbonViewModel : ViewModel
     {
+        private double m_zoom;
+
         public RibbonViewModel(IEventAggregator ea)
         {
             AboutCommand = new DelegateCommand(() => ea.GetEvent<AboutCommandEvent>().Publish());
+            Items = new double[] { 10, 20, 30, 30, 40, 50 };
+            Zoom = 50;
         }
 
         ICommand AddButton { get; set; }
@@ -17,6 +21,18 @@ namespace ModuleA.ViewModels
         public DelegateCommand DeleteCommand {get;set;}
 
         public DelegateCommand AboutCommand { get; set; }
+
+        public double[] Items { get; set; }
+
+        public double Zoom
+        {
+            get { return m_zoom; }
+            set
+            {
+                m_zoom = value;
+                OnPropertyChanged("Zoom");
+            }
+        }
 
     }
 }
